1fe6dd74508b8ae7fdf1cae30e22388594a17701
SUYEON KIM <yeonee230@gmail.com>
2023-01-31 13:50:39 +0900

    make init.js
    서버의 역할만 할 수 있도록 파일을 분리함.

---
+++ src/init.js
@@ -0,0 +1,10 @@
+import "./db";
+import "./models/Video";
+import app from "./server.js";
+
+const PORT = "4000";
+//외부 접속을 listening함. 
+const handleListening = () => console.log(`server listening on port ${PORT}`);
+
+app.listen(PORT, handleListening); // 서버가 port 4000의 request(handleListening 콜백) 를 받는다. 
+//listen() is the code that opens the port to listen for connections.
--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
-import "./db";
-import "./models/Video";
+
 import express from "express";
 import morgan from "morgan";
 //라우터 폴터에서 각 라우터들을 import한다. 
@@ -8,7 +7,7 @@ import userRouter from "./routers/userRouters";
 import videoRouter from "./routers/videoRouters";
 
 const app = express(); //서버를 만들었다. 
-const PORT = "4000";
+
 const logger = morgan("dev");// logger middleware 만든다. 
 
 //view engine 세팅 
@@ -18,17 +17,14 @@ app.set("views", process.cwd() + "/src/views"); //현재작업폴더(cwd) = node
 //morgan 사용 
 app.use(logger); 
 
-
 //큰주제가 되는 각 라우터들을 use()에 넣는다. 
 app.use("/",globalRouter);
 app.use("/users",userRouter);
 app.use("/videos",videoRouter);
 
+export default app;
 
 
 
-//외부 접속을 listening함. 
-const handleListening = () => console.log(`server listening on port ${PORT}`);
 
-app.listen(PORT, handleListening); // 서버가 port 4000의 request(handleListening 콜백) 를 받는다. 
 
