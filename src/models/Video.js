562bf11f6826bd8d5c3efb049bfff8e1d000f2f3
SUYEON KIM <yeonee230@gmail.com>
2023-01-31 13:29:30 +0900

    update video model

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 //model schema를 만들어준다. schema는 데이터가 어떤식으로 생겼는지 알려주는 것. 
-const videoSchema = mongoose.Schema({
+const videoSchema = new mongoose.Schema({
     title : String,
     description : String,
     createdAt : Date,
