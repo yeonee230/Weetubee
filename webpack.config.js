5fdd7367fac376d954866e044d6fe1620c477015
SUYEON KIM <yeonee230@gmail.com>
2023-02-04 19:03:50 +0900

    add webpack MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: "./src/client/js/main.js",
     mode: "development",
+    plugins: [new MiniCssExtractPlugin({
+      filename : "css/styles.css"
+    })],
     output: {
-      filename: "main.js",
-      path: path.resolve(__dirname, "assets", "js"),
+      filename: "js/main.js",
+      path: path.resolve(__dirname, "assets"),
     },
     module : {
         rules : [
@@ -21,7 +25,7 @@ module.exports = {
             },
             {
               test: /\.scss$/,
-              use: ["style-loader", "css-loader", "sass-loader"],
+              use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
             },
         ],
     },
