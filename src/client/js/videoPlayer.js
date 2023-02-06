32d4d047c6d0f901ce351bd566b30dcdc0eba527
SUYEON KIM <yeonee230@gmail.com>
2023-02-06 21:25:59 +0900

    Video player : volume
    Bug 있음..
    1. range 변경 없을 때 mute 버튼 동작 함.
    2. range >0 변경 있을 때 mute 했다가 다시 unmute 하면 이전 볼륨으로 돌아감.
    3. range=0일 때 unmute 버튼을 두번 눌러야 동작함 -> 버그

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,6 +4,9 @@ const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
 
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
 //Play
 const handlePlayClick = (event) =>{
     //if 클릭하면 플레이 
@@ -22,6 +25,7 @@ const handlePause =()=> {playBtn.innerText= "재생";}
 const handleMuteClick = (event) =>{
     if(video.muted){ //video 가 mute 상태면 클릭하면-> unmute 됨.
         video.muted = false;
+        video.volume = volumeValue; //*
         
     }else{
         video.muted = true;
@@ -29,12 +33,50 @@ const handleMuteClick = (event) =>{
     }
 
     muteBtn.innerText = video.muted ? "Unmute" : "Mute";
-    volumeRange.value = video.muted ? 0 : 0.5 ;
+    volumeRange.value = video.muted ? 0 : volumeValue ;
 
 }
+
+//Volume Range
+const handleVolumeRange = (event) =>{
+    //event.target.value에서 값 받을 수 있다. 
+    const {target: {value}} = event;
+    
+    if(video.muted){ //비디오가 mute상태일때 range변화가 있으면 
+        video.muted = false; // 다시 소리나고 
+        muteBtn.innerText = "Mute"; // 버튼도 바껴야함. 
+    }
+
+    if(Number(value) === 0){//*
+        video.muted == true;
+        muteBtn.innerText = "Unmute";
+    }
+
+    video.volume = value; // 실제 볼륨 변경 
+
+   
+
+    //volumeValue = value; // volumeValue 값 변경
+   
+
+}
+
+const handleChangeVolumeRange = (event) => { //*
+    const {
+    target: { value },
+    } = event;
+
+    if (value != 0) {
+    volumeValue = value;
+    }
+};
 
 playBtn.addEventListener("click", handlePlayClick);
 video.addEventListener("play", handlePlay);
 video.addEventListener("pause", handlePause);
 
 muteBtn.addEventListener("click", handleMuteClick);
+
+volumeRange.addEventListener("input", handleVolumeRange);
+volumeRange.addEventListener("change", handleChangeVolumeRange);
+
