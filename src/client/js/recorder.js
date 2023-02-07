302b1407701f6f62b977a7b9084ff4e3d49d93a7
SUYEON KIM <yeonee230@gmail.com>
2023-02-08 00:42:34 +0900

    Video thumbnail download finished

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,22 +1,38 @@
 import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 
-const startBtn = document.getElementById("startBtn");
+const actionBtn = document.getElementById("actionBtn");
 const video = document.getElementById("preview");
 
 let stream;
 let mediaRecorder;
 let videoFile;
 
+const downloadFile = (fileUrl, fileName) => {
+    const a = document.createElement("a");
+    a.href=fileUrl;
+    a.download= fileName;
+    document.body.appendChild(a);
+    a.click();
+};
+
 const handleDownload = async () => {
+    actionBtn.removeEventListener("click",handleDownload);
+    actionBtn.innerText = "Transcoding...";
+    actionBtn.disabled = true;
+    const file ={
+        input:"recording.webm",
+        output:"output.mp4",
+        thumb:"thumbnail.jpg",
+    };
 
     const ffmpeg = createFFmpeg({ log: true });
     await ffmpeg.load();
-    ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
-    await ffmpeg.run("-i", "recording.webm", "-r", "60","output.mp4"); //영상을 초당 60프레임으로 인코딩
-    await ffmpeg.run("-i", "recording.webm", "-ss", "00:00:01", "-frames:v","1","thumbnail.jpg");
+    ffmpeg.FS("writeFile", file.input, await fetchFile(videoFile));
+    await ffmpeg.run("-i", file.input, "-r", "60",file.output); //영상을 초당 60프레임으로 인코딩
+    await ffmpeg.run("-i", file.input, "-ss", "00:00:01", "-frames:v","1", file.thumb);
 
-    const mp4File = ffmpeg.FS("readFile","output.mp4");
-    const thumbFile = ffmpeg.FS("readFile","thumbnail.jpg");
+    const mp4File = ffmpeg.FS("readFile",file.output);
+    const thumbFile = ffmpeg.FS("readFile", file.thumb);
 
     const mp4Blob = new Blob([mp4File.buffer],{type: "video/mp4"});
     const thumbBlob = new Blob([thumbFile.buffer],{type: "image/jpg"});
@@ -24,27 +40,30 @@ const handleDownload = async () => {
     const mp4Url = URL.createObjectURL(mp4Blob);
     const thumbUrl = URL.createObjectURL(thumbBlob);
 
-    const a = document.createElement("a");
-    a.href=mp4Url;
-    a.download="MyVideo.mp4";
-    document.body.appendChild(a);
-    a.click();
-
-    const thumbA = document.createElement("a");
-    thumbA.href=thumbUrl;
-    thumbA.download="thumbnail.jpg";
-    document.body.appendChild(thumbA);
-    thumbA.click();
+    downloadFile(mp4Url,"MyVideo.mp4");
+    downloadFile(thumbUrl,"MyThumbnail.jpg");
+    
     //Closing camera after downloading
     // video.pause();
     // video.src = "";
     // stream.getTracks()[0].stop(); 
+
+    ffmpeg.FS("unlink", file.input);
+    ffmpeg.FS("unlink", file.output);
+    ffmpeg.FS("unlink", file.thumb);
+    URL.revokeObjectURL(mp4Url);
+    URL.revokeObjectURL(thumbUrl);
+    URL.revokeObjectURL(videoFile);
+    
+    actionBtn.innerText = "Start Recording";
+    actionBtn.disabled = false;
+    actionBtn.addEventListener("click",handleStart);
 };
 
-const handleStartBtn = () => {
-    startBtn.innerText="Stop Recording";
-    startBtn.removeEventListener("click", handleStartBtn);
-    startBtn.addEventListener("click", handleStop);
+const handleStart = () => {
+    actionBtn.innerText="Stop Recording";
+    actionBtn.removeEventListener("click", handleStart);
+    actionBtn.addEventListener("click", handleStop);
     mediaRecorder = new MediaRecorder(stream);
 
     mediaRecorder.ondataavailable = (event) => {
@@ -59,9 +78,9 @@ const handleStartBtn = () => {
 };
 
 const handleStop = () => {
-    startBtn.innerText="Download Recording";
-    startBtn.removeEventListener("click", handleStop);
-    startBtn.addEventListener("click", handleDownload);
+    actionBtn.innerText="Download Recording";
+    actionBtn.removeEventListener("click", handleStop);
+    actionBtn.addEventListener("click", handleDownload);
     mediaRecorder.stop();
 };
 
@@ -79,4 +98,4 @@ const init = async () => {
 };
 init()
 
-startBtn.addEventListener("click", handleStartBtn);
+actionBtn.addEventListener("click", handleStart);
