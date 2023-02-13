abfa70c6c08c68dda400fc49228eb2324846ce88
SUYEON KIM <yeonee230@gmail.com>
2023-02-13 16:35:01 +0900

    Comments : DB set up

---
+++ src/models/Comment.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+    createdAt :  { type: Date , required : true, default: Date.now},
+    text : {type: String, required :true},
+    owner :{ type : mongoose.Schema.Types.ObjectId , required: true, ref : "User"},
+    video :{ type : mongoose.Schema.Types.ObjectId , required: true, ref : "Video"},
+});
+
+const commentModel = mongoose.model("Comment",commentSchema);
+export default commentModel;
