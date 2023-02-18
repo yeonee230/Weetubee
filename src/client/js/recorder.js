245880be9d86fe4f9104d905b943d88f790b63af
SUYEON KIM <yeonee230@gmail.com>
2023-02-18 18:44:50 +0900

    fix recorder

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -22,37 +22,38 @@ const handleDownload = async () => {
     const file ={
         input:"recording.webm",
         output:"output.mp4",
-        thumb:"thumbnail.jpg",
+        //thumb:"thumbnail.jpg",
     };
 
     const ffmpeg = createFFmpeg({ log: true });
     await ffmpeg.load();
-    await ffmpeg.FS("writeFile", file.input, await fetchFile(videoFile));
+    ffmpeg.FS("writeFile", file.input, await fetchFile(videoFile));
     await ffmpeg.run("-i", file.input, "-r", "60",file.output); //영상을 초당 60프레임으로 인코딩
-    await ffmpeg.run("-i", file.input, "-ss", "00:00:01", "-frames:v","1", file.thumb);
+    //await ffmpeg.run("-i", file.input, "-ss", "00:00:01", "-frames:v","1", file.thumb);
 
-    const mp4File = ffmpeg.FS("readFile",file.output);
-    const thumbFile = ffmpeg.FS("readFile", file.thumb);
+    const mp4File = ffmpeg.FS("readFile",file.output); 
+    //const thumbFile = ffmpeg.FS("readFile", file.thumb);////FIXME: thumbnail 만들 때 여기서 에러 생김.. 
+
 
     const mp4Blob = new Blob([mp4File.buffer],{type: "video/mp4"});
-    const thumbBlob = new Blob([thumbFile.buffer],{type: "image/jpg"});
+    //const thumbBlob = new Blob([thumbFile.buffer],{type: "image/jpg"});
 
     const mp4Url = URL.createObjectURL(mp4Blob);
-    const thumbUrl = URL.createObjectURL(thumbBlob);
+    //const thumbUrl = URL.createObjectURL(thumbBlob);
 
     downloadFile(mp4Url,"MyVideo.mp4");
-    downloadFile(thumbUrl,"MyThumbnail.jpg");
+    //downloadFile(thumbUrl,"MyThumbnail.jpg");
     
-    //Closing camera after downloading
+    // Closing camera after downloading
     // video.pause();
     // video.src = "";
     // stream.getTracks()[0].stop(); 
 
     ffmpeg.FS("unlink", file.input);
     ffmpeg.FS("unlink", file.output);
-    ffmpeg.FS("unlink", file.thumb);
+    //ffmpeg.FS("unlink", file.thumb);
     URL.revokeObjectURL(mp4Url);
-    URL.revokeObjectURL(thumbUrl);
+    //URL.revokeObjectURL(thumbUrl);
     URL.revokeObjectURL(videoFile);
     
     actionBtn.disabled = false;
