28e275ffce8d7400288bdd701939a99ffb58c316
SUYEON KIM <yeonee230@gmail.com>
2023-02-18 17:33:12 +0900

    fix bug

--- src/server.js
+++ src/server.js
@@ -42,12 +42,31 @@ app.use(flash());
 app.use("/uploads",express.static("uploads"));
 app.use("/static",express.static("assets"));
 
-app.use((req, res, next) => {
-    res.header("Cross-Origin-Embedder-Policy", "require-corp");
-    res.header("Cross-Origin-Opener-Policy", "same-origin");
-    next();
-    });
+// app.use((req, res, next) => {
+//     res.header("Cross-Origin-Embedder-Policy", "require-corp");
+//     res.header("Cross-Origin-Opener-Policy", "same-origin");
+//     next();
+//     });
 
+app.use((req, res, next) => {
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    
+    res.header(
+    
+    "Access-Control-Allow-Headers",
+    
+    "Origin, X-Requested-With, Content-Type, Accept"
+    
+    );
+    
+    res.header("Cross-Origin-Embedder-Policy", "credentialless");
+    
+    res.header("Cross-Origin-Opener-Policy", "same-origin");
+    
+    next();
+    
+    })
 
 //큰주제가 되는 각 라우터들을 use()에 넣는다. 
 app.use("/",rootRouter);
